9a8d360acba67c9deb78c45d42c7551aa7d7de6a
dkomin415 <dkomin415.sales@gmail.com>
2022-07-25 14:49:20 -0700

    provided validation for required questions

--- app.js
+++ app.js
@@ -15,12 +15,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username:'
+      message: 'Enter your GitHub Username (Required):',
+      validate: usernameInput => {
+        if (usernameInput) {
+          return true;
+        } else {
+          console.log('Please enter your username!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -46,17 +62,28 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project (Required)?',
+      validate: projectName => {
+        if (projectName) {
+          return true;
+        } else {
+          console.log("Please enter your project's name!");
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: projectDescript => {
+        if (projectDescript) {
+          return true;
+        } else {
+          console.log("Please enter a description for your project!");
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -67,7 +94,15 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: githubLink => {
+        if (githubLink) {
+          return true;
+        } else {
+          console.log("Please enter a link to your project!");
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
